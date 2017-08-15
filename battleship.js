2421bd020ae9a1ccf4bfba319f2597574ad95637
Uspehov <muspehov@mail.ru>
2017-08-16 09:28:18 +1000

    Ошибки исправлены. Как в книге. За сключением сокращений операторов.

--- battleship.js
+++ battleship.js
@@ -7,23 +7,23 @@ var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while (isSunk) {
+while (isSunk == false) {
     guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-    if (guess < 0 || gess > 6) {
-        console.log("Please enter a valid cell number!");
-    } else guesses++ ;
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses++;
 
-    if (guess == location1 || guess == location2 || guess == location3) {
-        console.log("HIT!");
-        hits++;
-        if (hits == 3) {
-            isSunk = true;
-            console.log("You sank my battleship!");
-        } else {
-            console.log("MISS");
-        }
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
+            hits++;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        } else alert("MISS");
     }
 }
 
 var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was  " + (3 / guesses);
-console.log(stats);
+alert(stats);
