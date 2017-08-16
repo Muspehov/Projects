6443a38e8436c42cb8fb1a4d5d0367dff57d56db
Uspehov <muspehov@mail.ru>
2017-08-16 11:32:52 +1000

    Свой вариант. Учет вторичных попаданий.

--- battleship.js
+++ battleship.js
@@ -1,6 +1,6 @@
-var location1 = 3,
-    location2 = 4,
-    location3 = 5;
+var location1 = [3, false],
+    location2 = [4, false],
+    location3 = [5, false];
 
 var guess;
 var hits = 0;
@@ -13,17 +13,47 @@ while (isSunk == false) {
         alert("Please enter a valid cell number!");
     } else {
         guesses++;
-
-        if (guess == location1 || guess == location2 || guess == location3) {
-            alert("HIT!");
-            hits++;
-            if (hits == 3) {
-                isSunk = true;
-                alert("You sank my battleship!");
+        if (guess == location1[0]) {
+            if (!location1[1]) {
+                location1[1] = true;
+                alert("HIT!");
+                hits++;
+                if (hits == 3) {
+                    isSunk = true;
+                    alert("You sank my battleship!");
+                }
+            } else {
+                alert("Hit into the same place of the ship");
             }
-        } else alert("MISS");
+        }
+        else if (guess == location2[0]) {
+            if (!location2[1]) {
+                location2[1] = true;
+                alert("HIT!");
+                hits++;
+                if (hits == 3) {
+                    isSunk = true;
+                    alert("You sank my battleship!");
+                }
+            } else {
+                alert("Hit into the same place of the ship");
+            }
+        }
+        else if (guess == location3[0]) {
+            if (!location3[1]) {
+                location3[1] = true;
+                alert("HIT!");
+                hits++;
+                if (hits == 3) {
+                    isSunk = true;
+                    alert("You sank my battleship!");
+                }
+            } else {
+                alert("Hit into the same place of the ship");
+            }
+        }
+        else alert("MISS");
     }
 }
-
 var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was  " + (3 / guesses);
 alert(stats);
